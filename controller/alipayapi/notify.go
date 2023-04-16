11333c03f9bc2ffc683813fdddbacdb63cb9d89b
stalary <stalary@163.com>
2023-04-16 23:33:36 +0800

    MOD: 边界问题

--- controller/alipayapi/notify.go
+++ controller/alipayapi/notify.go
@@ -47,8 +47,13 @@ func Notify(c *gin.Context) {
 		return
 	}
 	existOrder, err := order.GetOrderById(parseOrderId)
-	if err != nil || existOrder == nil {
-		tlog.Handler.Errorf(c, consts.SLTagHTTPFailed, "order=%s not found, error: %s", orderId, err.Error())
+	if err != nil {
+		tlog.Handler.Errorf(c, consts.SLTagHTTPFailed, "get order error, error: %s", err.Error())
+		c.String(http.StatusOK, "failed")
+		return
+	}
+	if existOrder == nil {
+		tlog.Handler.Errorf(c, consts.SLTagHTTPFailed, "orderId=%s not found order", orderId)
 		c.String(http.StatusOK, "failed")
 		return
 	}
@@ -95,9 +100,15 @@ func Notify(c *gin.Context) {
 		}
 		userId := existOrder.FromUserId
 		user, err := userapi.GetUserById(userId)
-		if err != nil || user == nil {
+		if err != nil {
 			tx.Rollback()
 			tlog.Handler.Errorf(c, consts.SLTagHTTPFailed, "userId=%d not found user, error: %s", userId, err.Error())
+			c.String(http.StatusOK, "failed")
+			return
+		}
+		if user == nil {
+			tx.Rollback()
+			tlog.Handler.Errorf(c, consts.SLTagHTTPFailed, "userId=%d not found user", userId)
 			c.String(http.StatusOK, "failed")
 			return
 		}
