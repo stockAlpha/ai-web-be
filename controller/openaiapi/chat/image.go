fae6f8bc2cfad5f90b22305d7ac3675a4947ab6f
stalary <stalary@163.com>
2023-04-18 09:45:15 +0800

    ADD: 增加计费功能

--- controller/openaiapi/chat/image.go
+++ controller/openaiapi/chat/image.go
@@ -10,6 +10,8 @@ import (
 	"stock-web-be/gocommon/conf"
 	"stock-web-be/gocommon/consts"
 	"stock-web-be/gocommon/tlog"
+	"stock-web-be/logic/userapi"
+	"strconv"
 )
 
 // @Tags	代理OpenAI相关接口
@@ -20,6 +22,7 @@ func Image(c *gin.Context) {
 	cg := controller.Gin{Ctx: c}
 	ctx := context.Background()
 	apiKey := conf.Handler.GetString(`openai.key`)
+	userId, _ := strconv.ParseUint(c.GetString("user_id"), 10, 64)
 	client := openai.NewClient(apiKey)
 	var req openai.ImageRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -31,6 +34,25 @@ func Image(c *gin.Context) {
 	if err != nil {
 		fmt.Printf("Image creation error: %v\n", err)
 		cg.Res(http.StatusBadRequest, controller.ErrnoInvalidPrm)
+		return
+	}
+	cg.Resp(http.StatusOK, controller.ErrnoSuccess, respUrl.Data[0].URL)
+	// 计费
+	amount := req.N
+	switch req.Size {
+	case "256×256":
+		amount = amount * 2
+	case "512×512":
+		amount = amount * 3
+	case "1024×1024":
+		amount = amount * 4
+	default:
+		amount = amount * 2
+	}
+	err = userapi.SubUserIntegral(userId, amount)
+	if err != nil {
+		tlog.Handler.Errorf(c, consts.SLTagHTTPFailed, "record user integral error: %s", err.Error())
+		cg.Res(http.StatusBadRequest, controller.ErrServer)
 		return
 	}
 	cg.Resp(http.StatusOK, controller.ErrnoSuccess, respUrl.Data[0].URL)
