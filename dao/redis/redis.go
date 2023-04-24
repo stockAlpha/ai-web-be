32781efd4585a3507d495a87805130b17cccdf00
Stalary <stalary@163.com>
2023-04-24 17:56:41 +0800

    MOD: 改为使用补偿机制 (#20)
    * MOD: 计费改为使用补偿机制，事务占用链接较长时间

--- dao/redis/redis.go
+++ dao/redis/redis.go
@@ -26,20 +26,13 @@ func Init() {
 		Addr:     address,
 		Password: password,
 	}
-	if conf.Handler.GetString("redis.password") != "" {
-		redisOption.Password = conf.Handler.GetString("redis.password")
-	}
-	if conf.Handler.GetString("redis.default_db") != "" {
-		redisOption.DB = conf.Handler.GetInt("redis.default_db")
-	}
 
-	tlog.Handler.Infof(nil, consts.SLTagRedisSuccess, redisOption.Addr+","+redisOption.Password)
 	redisClient = redis.NewClient(redisOption)
 	if resp := redisClient.Ping(context.Background()); resp.Err() != nil {
 		panic("redis init error," + redisOption.Addr + "," + redisOption.Password)
 	}
-
 	tlog.Handler.Infof(nil, consts.SLTagRedisSuccess, redisOption.Addr+","+redisOption.Password)
+
 }
 
 func Close() {
