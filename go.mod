006e1b1f01125eeb9426d4320a6ddbff8d4fb4a5
stalary <stalary@163.com>
2023-04-30 18:37:53 +0800

    MOD: 替换dalle2返回的url为阿里云oss的地址

--- go.mod
+++ go.mod
@@ -26,6 +26,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/aliyun/aliyun-oss-go-sdk v0.0.0-20230323083302-77977ff44f38 // indirect
 	github.com/bytedance/sonic v1.8.7 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
@@ -74,6 +75,7 @@ require (
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/time v0.1.0 // indirect
 	golang.org/x/tools v0.8.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
---
+++ logic/aliyunapi/oss.go
@@ -0,0 +1,32 @@
+package aliyunapi
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/google/uuid"
+	"io"
+	"net/http"
+	"stock-web-be/client/ossclient"
+	"stock-web-be/gocommon/consts"
+	"stock-web-be/gocommon/tlog"
+	"time"
+)
+
+func UploadFileByUrl(url string) string {
+	bucket := ossclient.GetOssBucket()
+	resp, err := http.Get(url)
+	if err != nil {
+		tlog.Handler.Errorf(nil, consts.SLTagHTTPFailed, "get url error url=%s", url)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	content, err := io.ReadAll(resp.Body)
+	fileName := fmt.Sprintf("image/img-%s-%s", uuid.New().String(), time.Now().Format("2006-01-02"))
+	err = bucket.PutObject(fileName, bytes.NewReader(content))
+	if err != nil {
+		tlog.Handler.Errorf(nil, consts.SLTagHTTPFailed, "put object error url=%s", url, err.Error())
+		return ""
+	}
+	return "https://chatalpha.oss-cn-beijing.aliyuncs.com/" + fileName
+}
