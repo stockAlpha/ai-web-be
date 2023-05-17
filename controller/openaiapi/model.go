8982c8b4c2cd1c6470025dda04487f11a30875d4
stalary <stalary@163.com>
2023-05-17 09:48:53 +0800

    MOD: 放开sd画图限制

--- controller/openaiapi/model.go
+++ controller/openaiapi/model.go
@@ -4,8 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"stock-web-be/controller"
-	"stock-web-be/logic/userapi"
-	"strconv"
 )
 
 // @Tags	OpenAI相关接口
@@ -14,13 +12,9 @@ import (
 // @Response 200 {object} map[string][]string 模型列表
 func Model(c *gin.Context) {
 	cg := controller.Gin{Ctx: c}
-	userId, _ := strconv.ParseUint(c.GetString("user_id"), 10, 64)
-	userProfile, _ := userapi.GetUserById(userId)
-	imageModel := []string{"dall-e2"}
-	// vip用户可以优先体验sd
-	if userProfile.VipUser {
-		imageModel = []string{"dall-e2", "stable-diffusion"}
-	}
+	//userId, _ := strconv.ParseUint(c.GetString("user_id"), 10, 64)
+	//userProfile, _ := userapi.GetUserById(userId)
+	imageModel := []string{"dall-e2", "stable-diffusion"}
 	model := make(map[string][]string)
 	model["chat"] = []string{"gpt-3.5-turbo"}
 	model["image"] = imageModel
